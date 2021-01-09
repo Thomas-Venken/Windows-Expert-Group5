810e21e01c88c68aa29e9195d63c0ad70c2ff031
Jens Caenen <Jens.Caenen@student.pxl.be>
2021-01-09 14:55:07 +0100

    Added powershell script

--- setup/powershell.ps1
+++ setup/powershell.ps1
@@ -1,2 +1,2 @@
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName "CompanyX.local" -CreateDNSDelegation -DomainMode Win2012R2 -ForestMode Win2012R2 -SafeAdministratorPassword (ConvertTo-SecureString "Pass1234" -AsPlainText -Force)
+Install-ADDSForest -DomainName "CompanyX.local" -InstallDNS -DomainMode Win2012R2 -ForestMode Win2012R2 -SafeAdministratorPassword (ConvertTo-SecureString "Pass1234" -AsPlainText -Force) -Confirm:$false
