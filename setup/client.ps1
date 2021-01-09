3363d439e2c836f81d2bb39e68465ab37910f842
Thomas Venken <11700525@student.pxl.be>
2021-01-09 16:38:52 +0100

    general bugfixes

--- setup/client.ps1
+++ setup/client.ps1
@@ -1,4 +1,4 @@
-New-NetIPAddress -InterfaceAlias "Ethernet0" -IPAddress 172.16.60.191 -PrefixLength 24 -DefaultGateway 172.16.60.1
+New-NetIPAddress -InterfaceAlias "Ethernet0" -IPAddress 172.16.60.191 -PrefixLength 22 -DefaultGateway 172.16.60.1
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses 172.16.60.190, 8.8.8.8
 $username = "companyx\administrator"
 $password = (ConvertTo-SecureString "Pass1234" -AsPlainText -Force)
