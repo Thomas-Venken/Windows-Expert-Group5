ef81a4d1b031be8a13ff546028f3e7abb2159edf
Thomas Venken <11700525@student.pxl.be>
2021-01-09 15:54:33 +0100

    edit scripts

--- setup/client.ps1
+++ setup/client.ps1
@@ -1,5 +1,5 @@
-New-NetIPAddress -InterfaceAlias "Ethernet0" -IPAddress 10.128.12.118 -PrefixLength 24 -DefaultGateway 10.128.12.1
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses 1.1.1.1, 8.8.8.8
+New-NetIPAddress -InterfaceAlias "Ethernet0" -IPAddress 172.16.60.191 -PrefixLength 24 -DefaultGateway 172.16.60.1
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses 172.16.60.190, 8.8.8.8
 $username = "companyx\administrator"
 $password = (ConvertTo-SecureString "Pass1234" -AsPlainText -Force)
 $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
