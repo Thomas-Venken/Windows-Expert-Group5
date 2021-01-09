d540c97e3871f37044083bb41c0f5343cf3a1179
Thomas Venken <11700525@student.pxl.be>
2021-01-09 17:41:19 +0100

    powershell edit

--- setup/server.ps1
+++ setup/server.ps1
@@ -1,4 +1,4 @@
 New-NetIPAddress -InterfaceAlias "Ethernet0" -IPAddress 172.16.60.190 -PrefixLength 22 -DefaultGateway 172.16.60.1
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet0" -ServerAddresses 1.1.1.1, 8.8.8.8
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName "CompanyX.local" -InstallDNS -DomainMode Win2012R2 -ForestMode Win2012R2 -SafeAdministratorPassword (ConvertTo-SecureString "Pass1234" -AsPlainText -Force) -Confirm:$false
+Install-ADDSForest -DomainName "CompanyX.local" -InstallDNS -DomainMode Win2012R2 -ForestMode Win2012R2 -SafeModeAdministratorPassword (ConvertTo-SecureString "Pass1234" -AsPlainText -Force) -Confirm:$false
